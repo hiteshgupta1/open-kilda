1cf30783dcebf42d5c4926b5d497e6de845a74ad
Sergii Iakovenko <siakovenko@mirantis.com>
2018-07-09 16:59:29 +0300

    Avoid force rediscovery during FL-WFM synchronization process.

--- services/src/floodlight-modules/src/main/java/org/openkilda/floodlight/converter/IOFSwitchConverter.java
+++ services/src/floodlight-modules/src/main/java/org/openkilda/floodlight/converter/IOFSwitchConverter.java
@@ -1,11 +1,28 @@
+/* Copyright 2018 Telstra Open Source
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
 package org.openkilda.floodlight.converter;
 
-import net.floodlightcontroller.core.IOFSwitch;
-import net.floodlightcontroller.core.LogicalOFMessageCategory;
+import org.openkilda.messaging.info.discovery.NetworkDumpSwitchData;
 import org.openkilda.messaging.info.event.SwitchInfoData;
 import org.openkilda.messaging.info.event.SwitchInfoExtendedData;
 import org.openkilda.messaging.info.event.SwitchState;
 import org.openkilda.messaging.info.rule.FlowEntry;
+
+import net.floodlightcontroller.core.IOFSwitch;
+import net.floodlightcontroller.core.LogicalOFMessageCategory;
 import org.projectfloodlight.openflow.protocol.OFFlowStatsReply;
 
 import java.net.InetSocketAddress;
@@ -18,15 +35,26 @@ import java.util.stream.Collectors;
 public final class IOFSwitchConverter {
 
     /**
-     * Transforms {@link IOFSwitch} to object that is used throughout kilda in all components.
+     * Transforms {@link IOFSwitch} to {@link NetworkDumpSwitchData} object.
+     *
      * @param sw switch data.
+     * @return converted switch.
+     */
+    public static NetworkDumpSwitchData convertToNetworkDumpSwitchData(IOFSwitch sw) {
+        return new NetworkDumpSwitchData(sw.getId().toString());
+    }
+
+    /**
+     * Transforms {@link IOFSwitch} to {@link SwitchInfoData} object.
+     *
+     * @param sw        switch data.
      * @param eventType switch state.
      * @return converted switch.
      */
     public static SwitchInfoData buildSwitchInfoData(IOFSwitch sw, SwitchState eventType) {
         String switchId = sw.getId().toString();
         InetSocketAddress address = (InetSocketAddress) sw.getInetAddress();
-        InetSocketAddress controller =(InetSocketAddress) sw.getConnectionByCategory(
+        InetSocketAddress controller = (InetSocketAddress) sw.getConnectionByCategory(
                 LogicalOFMessageCategory.MAIN).getRemoteInetAddress();
 
         return new SwitchInfoData(
@@ -44,8 +72,9 @@ public final class IOFSwitchConverter {
     }
 
     /**
-     * Transforms {@link IOFSwitch} to object that is used throughout kilda in all components.
-     * @param sw switch data.
+     * Transforms {@link IOFSwitch} to {@link SwitchInfoExtendedData} object.
+     *
+     * @param sw        switch data.
      * @param eventType switch state.
      * @param flowStats installed flows.
      * @return converted switch.
